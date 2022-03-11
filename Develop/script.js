352dc591b6f0ce3d2aeb845f46ae2c12716b5f34
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-03-11 14:00:52 -0500

    added code to generate random passwords without specific requirements

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
+var passwordLength = window.prompt("Please enter the amout of characters you want to generate for the password (at least 8 and no more than 128 characters)");
+var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890'
+var password = '';
 
+var generatePassword = function (event) {
+if(passwordLength >= 8 && passwordLength <=128){
+  for(let i=0; i<passwordLength; i++) {
+      password = password + characters.charAt(Math.floor(Math.random()*characters.length));
+  }
+  console.log (password);
+  return password;
+ }
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
